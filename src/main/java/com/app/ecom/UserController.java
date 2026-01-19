e93fe091d144506b4ba1300030b6e507af61c494
srupeshreddy <sadirupeshr@gmail.com>
2026-01-19 09:09:31 +0530

    unneccessary imports removed

--- src/main/java/com/app/ecom/UserController.java
+++ src/main/java/com/app/ecom/UserController.java
@@ -1,12 +1,10 @@
 package com.app.ecom;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
